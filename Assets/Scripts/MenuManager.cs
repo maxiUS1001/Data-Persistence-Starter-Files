532a33e1c51f0ab4bba826c8ddf071b7c0bee85b
Maximiliano <alex.fiero1997@gmail.com>
2022-07-29 17:49:23 +0300

    start menu added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    public string playerName;
+    public int score;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
